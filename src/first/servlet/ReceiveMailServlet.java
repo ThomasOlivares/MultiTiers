3cd4ecc45208d82b94a5374ce547b8afef510619
dry3ss <willvi31@yahoo.fr>
2017-02-23 13:00:21 +0100

    delete

--- src/first/servlet/ReceiveMailServlet.java
+++ src/first/servlet/ReceiveMailServlet.java
@@ -5,15 +5,17 @@ import java.io.PrintWriter;
 import java.util.Vector;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import mail.*;
 
-
+@WebServlet("/ReceiveMailServlet")
 public class ReceiveMailServlet extends HttpServlet {
 
     /**
 	 * 
 	 */
+	
 	private static final long serialVersionUID = 1L;
 	MailServer ms ;
 
@@ -27,9 +29,11 @@ public class ReceiveMailServlet extends HttpServlet {
     {
     	response.setContentType("text/html");
     	PrintWriter out = response.getWriter();
-    	String to =  request.getParameter("to");
-    	MailServer ms=MailServer.newInstance();
-    	Vector<Message> mess=ms.readAllMessages(to);
-	 
+    	String to =  request.getParameter("to");    	
+    	String return_=ms.removeMessages(to);
+    	out.println("Suppression des messages deja lus:");
+    	out.println(return_);
+    	out.println("<br> <a href=\"http://localhost:8080/mail/index.html\"> Go back to index.html</a>");
+        out.close();
     }
 }
